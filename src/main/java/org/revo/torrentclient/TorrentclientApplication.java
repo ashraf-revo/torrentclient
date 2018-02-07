8096d1426dc438cebb694f9b58dd45c01a2ad650
ashraf <ashraf1abdelrasool@gmail.com>
2018-02-07 21:52:16 +0200

    testedb

--- src/main/java/org/revo/torrentclient/TorrentclientApplication.java
+++ src/main/java/org/revo/torrentclient/TorrentclientApplication.java
@@ -1,22 +1,25 @@
 package org.revo.torrentclient;
 
-import bt.Bt;
-import bt.data.Storage;
-import bt.data.file.FileSystemStorage;
-import bt.runtime.BtClient;
-import bt.torrent.TorrentSessionState;
+import org.revo.torrent.bencode.BencodeDecode;
+import org.revo.torrent.bencode.BencodeException;
+import org.revo.torrent.torrent.Torrent;
+import org.revo.torrent.torrent.torrentdata.TorrentData;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 import reactor.core.publisher.Flux;
 
-import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.time.Duration;
+import java.util.Map;
+import java.util.UUID;
 
 @SpringBootApplication
 @RestController
@@ -27,17 +30,28 @@ public class TorrentclientApplication {
     }
 
     @PostMapping()
-    public void index(@RequestBody Torrent torrent) throws IOException {
-        Path temp = Files.createTempDirectory("temp");
-        System.out.println(temp);
-                Storage storage = new FileSystemStorage(temp);
-        BtClient client = Bt.client()
-                .storage(storage)
-                .magnet(torrent.getMurl())
-                .autoLoadModules()
-                .stopWhenDownloaded()
-                .build();
-        Flux<TorrentSessionState> from = Flux.from(it -> client.startAsync(it::onNext, 1000));
-        from.map(TorrentSessionState::getDownloaded).log().subscribe();
+    public void index(@RequestPart("file") MultipartFile multipartFile) throws IOException {
+        Path base = Files.createTempDirectory("temp");
+        Path temp = Paths.get(base.toString(), UUID.randomUUID().toString().replace("-", "") + ".torrent");
+        System.out.println(base);
+        Files.copy(multipartFile.getInputStream(), temp);
+        try {
+            Torrent torrent = new Torrent(base.toString(), getTorrentData(temp.toFile().toString()));
+            torrent.startTorrent();
+
+            Flux.interval(Duration.ofSeconds(1)).subscribe(it -> {
+                System.out.println(torrent.getDownloaded());
+            });
+
+
+        } catch (IOException | BencodeException ignored) {
+            System.out.println(ignored.getMessage());
+        }
+
     }
+
+    static TorrentData getTorrentData(String torrent) throws IOException, BencodeException {
+        return new TorrentData((Map<String, Object>) BencodeDecode.bDecodeStream(new FileInputStream(torrent)));
+    }
+
 }
